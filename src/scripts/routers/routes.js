54c088d227c46135b727b5ddbd5472c3e9c236e3
dadanghawari <muhammaddadanghawari@gmail.com>
2023-11-29 06:26:17 +0800

    add artikel page

--- src/scripts/routers/routes.js
+++ src/scripts/routers/routes.js
@@ -3,6 +3,7 @@ import Tips from '../views/pages/tips';
 import Tentang from '../views/pages/tentang';
 import Event from '../views/pages/event';
 import Diskusi from '../views/pages/diskusi';
+import Artikel from '../views/pages/artikel';
 
 const routes = {
   '/': Beranda,
@@ -11,6 +12,7 @@ const routes = {
   '/event': Event,
   '/diskusi': Diskusi,
   '/tentang': Tentang,
+  '/artikel': Artikel,
 };
 
 export default routes;
