252b314518a8cef50e98a3a0d8f2e6a443511808
dadanghawari <muhammaddadanghawari@gmail.com>
2023-12-09 08:38:25 +0800

    add pwa, fitur validasi regist dan beberapa lainnya

--- src/scripts/utils/create-account.js
+++ src/scripts/utils/create-account.js
@@ -1,7 +1,10 @@
 // Import necessary functions from Firebase
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set } from 'firebase/database';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
+import {
+  successPopUp, wrongFormatPass, wrongFormatEmail, wrongFormatUsername, emptyField, infoPopUp,
+} from '../views/templates/page-creator';
 
 // Import Firebase configuration
 import firebaseConfig from '../global/DB_CONFIG';
@@ -19,39 +22,128 @@ const validateEmail = (email) => {
 
 const validatePassword = (password) => password.length >= 6;
 
+const validateUsername = (username) => username.length >= 6;
+
 const register = () => {
+  const wrapper = document.querySelector('.bungkus');
   const username = document.getElementById('usernameRegist').value;
   const email = document.getElementById('emailRegist').value;
   const password = document.getElementById('passwordRegist').value;
 
-  if (validateEmail(email) === false || validatePassword(password) === false) {
-    alert('Email atau password salah');
+  const data = {
+    usernameInput: username,
+    emailInput: email,
+    passwordInput: password,
+  };
+
+  const inputUsername = document.getElementById('inputUsername');
+  const inputEmail = document.getElementById('inputEmail');
+  const inputPassword = document.getElementById('inputPassword');
+  const divElement = document.createElement('div');
+
+  if (username.length === 0 || password.length === 0 || email.length === 0) {
+    divElement.innerHTML = emptyField;
+
+    // Memeriksa apakah inputPassword memiliki anak elemen dengan konten divElement
+    if (Array.from(inputPassword.children)
+      .some((child) => child.innerHTML === divElement.innerHTML)) {
+      return;
+    }
+
+    inputPassword.appendChild(divElement);
+    setTimeout(() => {
+      inputPassword.removeChild(divElement);
+    }, 2000);
+    return;
+  }
+
+  if (validateUsername(username) === false) {
+    divElement.innerHTML = wrongFormatUsername;
+
+    if (Array.from(inputUsername.children)
+      .some((child) => child.innerHTML === divElement.innerHTML)) {
+      return;
+    }
+    inputUsername.appendChild(divElement);
+    return;
+  }
+
+  if (validateEmail(email) === false) {
+    divElement.innerHTML = wrongFormatEmail;
+
+    if (Array.from(inputEmail.children)
+      .some((child) => child.innerHTML === divElement.innerHTML)) {
+      return;
+    }
+    inputEmail.appendChild(divElement);
+    return;
+  }
+
+  if (validatePassword(password) === false) {
+    divElement.innerHTML = wrongFormatPass;
+
+    if (Array.from(inputPassword.children)
+      .some((child) => child.innerHTML === divElement.innerHTML)) {
+      return;
+    }
+    inputPassword.appendChild(divElement);
     return;
   }
 
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const { user } = userCredential;
+      auth.currentUser.displayName = username;
 
       // Simpan informasi pengguna ke Realtime Database
       const userData = {
         username,
         email,
+        displayName: username,
+        uid: user.uid,
       };
+
+      localStorage.setItem('user', JSON.stringify(userData));
 
       // Gunakan user.uid sebagai bagian dari path di database
       set(ref(database, `users/${user.uid}`), userData);
+      sendEmailVerification(auth.currentUser);
+      const usernya = auth.currentUser;
+      console.log(usernya);
+      console.log(`nama display: ${usernya.uid}`);
+      console.log(`nama display: ${usernya.email}`);
+      console.log(`nama display: ${usernya.displayName}`);
+      console.log(`verifikasi: ${usernya.emailVerified}`);
+      console.log(`verifikasi: ${usernya.uid}`);
     })
     .then(() => {
-      alert('User created');
-    })
-    .catch((error) => {
+      if (inputUsername.innerHTML.includes(divElement)) { inputUsername.removeChild(divElement); }
+      if (inputPassword.innerHTML.includes(divElement)) { inputPassword.removeChild(divElement); }
+      if (inputEmail.innerHTML.includes(divElement)) { inputEmail.removeChild(divElement); }
+      wrapper.innerHTML += successPopUp;
+      setTimeout(() => {
+        if (auth) { location.assign('/#/beranda'); }
+      }, 2000);
+    }).catch((error) => {
+      inputUsername.value = data.usernameInput;
+      inputPassword.value = data.passwordInput;
+      inputEmail.value = data.emailInput;
       const errorMessage = error.message;
       if (errorMessage === 'Firebase: Error (auth/email-already-in-use).') {
-        alert('Email telah terdaftar');
-      } else {
-        console.error('Error creating account:', error);
+        divElement.innerHTML = infoPopUp;
+
+        if (Array.from(wrapper.children)
+          .some((child) => child.innerHTML === divElement.innerHTML)) {
+          return;
+        }
+        wrapper.appendChild(divElement);
+
+        setTimeout(() => {
+          wrapper.removeChild(divElement);
+        }, 3000);
+        return;
       }
+      console.error('Error creating account:', error);
     });
 };
 
---
+++ src/scripts/views/templates/page-creator.js
@@ -0,0 +1,40 @@
+const successPopUp = `
+    <div class="success-container">
+    <div class="success-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: #50f050;transform: ;msFilter:;"><path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path></svg>
+    </div>
+    <div class="success-p">
+        <p>Pendaftaran Berhasil</p>
+    </div>
+    </div>
+`;
+
+const infoPopUp = `
+    <div class="success-container">
+    <div class="info-icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: rgba(0, 169, 255, 1);transform: ;msFilter:;"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="M11 11h2v6h-2zm0-4h2v2h-2z"></path></svg>
+    </div>
+    <div class="info-p">
+        <p>Email yang anda masukkan telah terdaftar, silahkan login</p>
+    </div>
+    </div>
+`;
+
+const wrongFormatUsername = `
+    <div class="error">Username minimal 6 karakter
+    </div>
+`;
+const wrongFormatPass = `
+    <div class="error">Password minimal 6 karakter</div>
+`;
+const wrongFormatEmail = `
+    <div class="error">Mohon gunakan format email yang benar</div>
+`;
+
+const emptyField = `
+    <div class="error">Mohon untuk mengisi seluruh field</div>
+`;
+
+export {
+  successPopUp, infoPopUp, wrongFormatPass, wrongFormatEmail, wrongFormatUsername, emptyField,
+};
