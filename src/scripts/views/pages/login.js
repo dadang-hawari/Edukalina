a354ee923459b63205726d491033dafe73a0185f
dadanghawari <muhammaddadanghawari@gmail.com>
2023-12-13 18:02:05 +0800

    add login with google dan beberapa perbaikan lainnya

--- src/scripts/views/pages/login.js
+++ src/scripts/views/pages/login.js
@@ -1,4 +1,4 @@
-import login from '../../utils/login-handler';
+import { login, loginWithGoogle } from '../../utils/login-handler';
 
 const Login = {
   async render() {
@@ -21,9 +21,9 @@ const Login = {
                   <svg id="hiddenSvg" fill="rgb(84, 82, 82)" class="hidden-menu" xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zm151 118.3C226 97.7 269.5 80 320 80c65.2 0 118.8 29.6 159.9 67.7C518.4 183.5 545 226 558.6 256c-12.6 28-36.6 66.8-70.9 100.9l-53.8-42.2c9.1-17.6 14.2-37.5 14.2-58.7c0-70.7-57.3-128-128-128c-32.2 0-61.7 11.9-84.2 31.5l-46.1-36.1zM394.9 284.2l-81.5-63.9c4.2-8.5 6.6-18.2 6.6-28.3c0-5.5-.7-10.9-2-16c.7 0 1.3 0 2 0c44.2 0 80 35.8 80 80c0 9.9-1.8 19.4-5.1 28.2zm51.3 163.3l-41.9-33C378.8 425.4 350.7 432 320 432c-65.2 0-118.8-29.6-159.9-67.7C121.6 328.5 95 286 81.4 256c8.3-18.4 21.5-41.5 39.4-64.8L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5zm-88-69.3L302 334c-23.5-5.4-43.1-21.2-53.7-42.3l-56.1-44.2c-.2 2.8-.3 5.6-.3 8.5c0 70.7 57.3 128 128 128c13.3 0 26.1-2 38.2-5.8z"/></svg>
                 </button>
             </div>   
-            <button class="btnLogin" type="submit">Masuk</button>
+            <button class="btnLogin" type="button">Masuk</button>
             <p>---------- atau ----------</p>
-            <button class="btnLoginGoogle">Google</button>
+            <button class="btnLoginGoogle" type="button">Google</button>
         </form>
         <p>Belum mempunyai akun? <a href="#/daftar"><span class="spanLogin">Daftar disini</span></a></p>
     </div>
@@ -38,8 +38,17 @@ const Login = {
     const pwEye = document.getElementById('pwEyeLogin');
     const passwordInput = document.getElementById('password');
     const btnLogin = document.querySelector('.btnLogin');
+    const btnLoginGoogle = document.querySelector('.btnLoginGoogle');
 
-    btnLogin.addEventListener('click', login);
+    btnLogin.addEventListener('click', (event) => {
+      event.preventDefault();
+      login();
+    });
+
+    btnLoginGoogle.addEventListener('click', (event) => {
+      event.preventDefault();
+      loginWithGoogle();
+    });
 
     pwEye.addEventListener('click', () => {
       const openSvg = document.getElementById('openSvg');
