82dc1ae9b485e6726f5dcc340316b73882c2656d
Malbamario <malbamario@gmail.com>
2023-12-13 13:23:57 +0800

    another try for timeout

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ const init = async () => {
   // const playlistsService = new PlaylistsService(collaborationsService);
 
   const server = Hapi.server({
-    port: process.env.PORT,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
